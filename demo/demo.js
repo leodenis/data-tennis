2336e3c959d159c7997dfe9c5f4ae6e4b6e4e884
Leo D <denisleo23@gmail.com>
2013-06-05 19:48:08 +0200

    img

---
+++ demo/demo.js
@@ -0,0 +1,22 @@
+/*
+ * FancyMoves - MovingBoxes demo script
+ */
+
+$(function(){
+
+
+	
+	$('#slider-three').movingBoxes({
+		startPanel   : 2,      // start with this panel
+		width        : 860,    // overall width of movingBoxes (not including navigation arrows)
+		panelWidth   : .45,     // current panel width adjusted to 70% of overall width
+		buildNav     : false,
+		fixedHeight  : true,// if true, navigation links will be added
+		
+	});
+	
+
+
+
+
+});
